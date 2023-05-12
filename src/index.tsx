5820a777398a2028b1c3332802b564e065cac839
HiteshKumawat <hiteshkumawat19@gmail.com>
2023-05-12 14:14:56 +0530

    landing UI

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ContextProvider } from './contexts/ContextProvider';
-import 'semantic-ui-css/semantic.min.css';
+//import 'semantic-ui-css/semantic.min.css';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <ContextProvider>
-    <App />
+      <App />
     </ContextProvider>
   </React.StrictMode>
 );
