98d842fbb904fd773601c6f58bb1a59e7123e291
Kunal <kunalsainics@gmail.com>
2023-05-12 17:33:18 +0530

    update cors middleware config

--- main.go
+++ main.go
@@ -26,7 +26,12 @@ func main() {
 	ConnectToMongo(config.MongoDbConnStr, config.MongoTimeout).Database(config.MongoDbDataBase)
 
 	app := fiber.New()
-	app.Use(cors.New())
+	app.Use(cors.New(cors.Config{
+		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+		AllowOrigins:     "*",
+		AllowCredentials: true,
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+	}))
 	router := app.Group("api/v1")
 
 	metaRouter := router.Group("metadata")
