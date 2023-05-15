8627c3451bf66370fc6a658a75035f1524b48c69
Mehul-Sharma-Git <mehul.sharma@loginradius.com>
2023-05-15 09:41:15 +0530

    added second template

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ const App = () => {
   const baseURL = "https://sendright.lrhackathon.com"
 
   const [templateText, setTemplateText] = useState({ senderName: "ABC Company", recipientName: "recipient", industry: "", templateType: "", language: "", brandLogoURL: "https://apidocs.lrcontent.com/images/loginradius-logo--horizontal-full-colour-on-white_196175e99f5cec6b654.20520541.png", sentiment: "Official", prompt: "", content: "" })
+  const [selectedTemplate, setSelectedTemplate] = useState("template1")
   useEffect(() => {
 
     axios.get(`${baseURL}/api/v1/metadata`).then((response: any) => {
@@ -62,7 +63,8 @@ const App = () => {
   const downloadTxtFile = () => {
     // console.log(templateText.content)
     const element = document.createElement("a");
-    const file = new Blob([document.getElementById('downloadable_template').innerHTML], { type: 'text/plain' });
+    const downloadID = selectedTemplate==='template1'?'downloadable_template1':'downloadable_template2'
+    const file = new Blob([document.getElementById(downloadID).innerHTML], { type: 'text/plain' });
     element.href = URL.createObjectURL(file);
     element.download = "email_template.txt";
     document.body.appendChild(element); // Required for this to work in FireFox
@@ -241,14 +243,18 @@ const App = () => {
             <div>&nbsp;</div>
             <div className="grow">&nbsp;</div>
             <div className="form-group  ">
-              <select className="py-2 pl-[11px] pr-4 rounded-md border border-solid border-gray-400 text-sm" placeholder="Network" data-form-type="other">
-                <option value="" >Choose Template </option>
-                <option value="Template1 ">Template  1</option>
-                <option value="Template2 ">Template  2</option>
+              <select className="py-2 pl-[11px] pr-4 rounded-md border border-solid border-gray-400 text-sm" placeholder="Network" data-form-type="other" onChange={(e) => {
+
+                setSelectedTemplate(e.target.value)
+
+              }} value={selectedTemplate} defaultValue={"template1"}>
+
+                <option value="template1 ">Template  1</option>
+                <option value="template2 ">Template  2</option>
               </select>
             </div>
           </div>
-          <div id='downloadable_template' className="overflow-y-auto  md:max-h-[83.5vh] h-auto bg-gray-50">
+          {selectedTemplate === 'template1' ? <div id='downloadable_template1' className="overflow-y-auto  md:max-h-[83.5vh] h-auto bg-gray-50">
             <div style={{ backgroundColor: "rgb(249 250 251)", padding: 24 }}>
               <center style={{ maxWidth: 600, marginInline: "auto" }}>
                 <table
@@ -672,7 +678,87 @@ const App = () => {
                 </table>
               </center>
             </div>
-          </div>
+          </div> : <div id='downloadable_template2' className="overflow-y-auto  md:max-h-[83.5vh] h-auto bg-gray-50">
+
+
+            <div style={{ backgroundColor: '#F5F7FA', padding: '24px', fontFamily: 'Arial, Helvetica, sans-serif' }}>
+              <center style={{ maxWidth: '600px', marginInline: 'auto' }}>
+                <table border={0} cellSpacing="0" cellPadding="0" style={{
+                  borderCollapse: 'collapse', backgroundColor: '#ffffff', boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.16)', textAlign: 'center',
+                  borderRadius: '3px'
+                }}>
+                  <tr>
+                    <td>
+                      <div style={{ textAlign: 'center', padding: '32px 24px 32px' }}>
+                        <img src={templateText.brandLogoURL} width={200} style={{ display: 'block', marginInline: 'auto' }} alt="logo"
+                          title="logo" />
+                      </div>
+                    </td>
+                  </tr>
+                  {templateText.content === "" ? contentLoading ? <tr><td><div style={{ width: '600px' }} className="text-center">
+                      <div style={{ margin: "20px 0 40px 0" }} role="status">
+                        <svg aria-hidden="true" className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                        </svg>
+                        <span className="sr-only">Loading...</span>
+                      </div>
+                    </div></td></tr> : <>
+                  <tr>
+                    <td>
+                      <b
+                        style={{ color: '#333333', fontSize: '16px', fontWeight: '600', padding: '0 24px', margin: '0 0 24px 0', display: 'block' }}>
+                        Dear
+                        {templateText.recipientName},</b>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <p
+                        style={{ color: '#4F4F4F', fontSize: '15px', padding: '0 24px', lineHeight: '1.5', margin: '0 0 32px 0' }}>
+                        Thank you for
+                        placing your
+                        order with SendRight!
+                        We're excited to
+                        let you know that your
+                        order has been confirmed and is being processed for delivery.</p>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <p
+                        style={{ color: '#4F4F4F', fontSize: '15px', padding: '0 24px', lineHeight: '1.5', margin: '0 0 32px 0' }}>
+                        If you have any questions or concerns about your order, please don't hesitate to reach
+                        out to our customer service team. You can reply to this email or contact us at <a
+                          href="tel:+91 1234567890">+91 1234567890</a>.</p>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td>
+                      <div style={{ padding: '0 24px 32px' }}>
+                        <table border={0} cellSpacing="0" cellPadding="0"
+                          style={{ borderCollapse: 'collapse', lineHeight: '1.5', width: '100%' }}>
+                          <tr>
+                            <td><b style={{ color: '#333333', fontWeight: '600' }}>Best regards,</b></td>
+                          </tr>
+                          
+                          <tr>
+                            <td>
+                              <p style={{ fontSize: '15px', margin: '0' }}>{templateText.senderName}</p>
+                            </td>
+                          </tr>
+                        </table>
+                      </div>
+                    </td>
+                  </tr>
+                  </> : <tr><td><p style={{ whiteSpace: "pre-line", padding: "0 24px 32px" }}>{`${templateText.content}`}</p></td></tr>}
+                </table>
+              </center>
+            </div>
+          </div>}
+
+
         </div>
       </div>
     </div>
